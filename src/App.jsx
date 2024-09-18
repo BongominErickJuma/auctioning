8bcb59a9025b7ba06aecd076d0d0ecc760efbb87
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-09-18 04:26:58 -0400

    protected

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import SignUp from "./pages/Signup/Signup";
 import Profile from "./pages/Profile/Profile";
 import Checkout from "./pages/Checkout/Checkout";
 import Auctioning from "./pages/Auction/Auctioning";
+import SubCategory from "./pages/Category/SubCategory";
+import ProtectedRoute from "./Contex/ProtectedRoute";
+
+
 const App = () => {
   return (
     <Router>
@@ -18,16 +22,26 @@ const App = () => {
       <div className="App default-padding mt-5">
         <Routes>
           <Route exact path="/auctioning" element={<Home />} />
-          <Route path="/auctioning/details" element={<Details />} />
+          <Route path="/auctioning/details/:id" element={<Details />} />
           <Route
             path="/auctioning/productDetail"
             element={<ProductDetails />}
           />
           <Route path="/auctioning/profile" element={<Profile />} />
-          <Route path="/auctioning/checkout" element={<Checkout />} />
           <Route path="/auctioning/auction" element={<Auctioning />} />
           <Route path="/auctioning/login" element={<Login />} />
           <Route path="/auctioning/signup" element={<SignUp />} />
+          <Route path="/auctioning/subcategory/:id" element={<SubCategory />} />
+          
+          {/* protected routes */}
+          <Route element={<ProtectedRoute/>}>
+
+          <Route path="/auctioning/checkout" element={<Checkout />} />
+
+          </Route>
+
+        
+
           <Route path="*" element={<h1>Page not found</h1>} />
         </Routes>
       </div>
