f96fe0121efd78076573504e20d8be7000bb86a4
BongominErickJuma <ericbongomin@gmail.com>
2024-09-18 14:19:40 +0300

    cmd

--- src/pages/Auction/Auctioning.jsx
+++ src/pages/Auction/Auctioning.jsx
@@ -62,20 +62,28 @@ const Auctioning = () => {
 
       <div className="row g-5">
         <div className="col-md-8">
-          <h3 className="pb-4 mb-4 fst-italic border-bottom">
-            From the Firehose
-          </h3>
-
           <article className="blog-post">
-            <h2 className="blog-post-title">Sample blog post</h2>
-            <p className="blog-post-meta">
-              January 1, 2021 by <a href="#">Mark</a>
-            </p>
-            <p>
-              This blog post shows a few different types of content that are
-              supported and styled with Bootstrap. Basic typography, lists,
-              tables, images, code, and more are all supported as expected.
-            </p>
+            <h2 className="blog-post-title">
+              2011 Sullair 900HAFDTQ-CA3 900 cfm Mobile Air Compressor
+            </h2>
+
+            <div className="flex-r">
+              <div className="border-end me-2 p-2">
+                <i className="bi bi-geo-alt"></i>
+                <p className="text-muted">Located</p>
+                <h5>Mont-Saint-Hilaire, QC, CAN</h5>
+              </div>
+              <div className="border-end me-2 p-2">
+                <i className="bi bi-alarm"></i>
+                <p className="text-muted">Meter</p>
+                <h5>11,074 hrs</h5>
+              </div>
+              <div className="border-end me-2 p-2">
+                <i className="bi bi-square"></i>
+                <p className="text-muted">Serial number</p>
+                <h5>201101080022</h5>
+              </div>
+            </div>
             <hr />
             <p>
               This is some additional paragraph placeholder content. It has been
@@ -145,43 +153,75 @@ const Auctioning = () => {
 
         <div className="col-md-4">
           <div className="position-sticky shadow" style={{ top: "2rem" }}>
-            <div className="p-4 mb-3 bg-light rounded">
-              <h4 className="fst-italic">About</h4>
-              <p className="mb-0">
-                Customize this section to tell your visitors a little bit about
-                your publication, writers, content, or something else entirely.
-              </p>
-            </div>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Bordered Tabs Justified</h5>
 
-            <div className="p-4">
-              <h4 className="fst-italic">Archives</h4>
-              <ol className="list-unstyled mb-0">
-                <li>
-                  <a href="#">March 2021</a>
-                </li>
-                <li>
-                  <a href="#">February 2021</a>
-                </li>
-                <li>
-                  <a href="#">January 2021</a>
-                </li>
-                {/* Additional list items */}
-              </ol>
-            </div>
-
-            <div className="p-4">
-              <h4 className="fst-italic">Elsewhere</h4>
-              <ol className="list-unstyled">
-                <li>
-                  <a href="#">GitHub</a>
-                </li>
-                <li>
-                  <a href="#">Twitter</a>
-                </li>
-                <li>
-                  <a href="#">Facebook</a>
-                </li>
-              </ol>
+                <ul
+                  className="nav nav-tabs nav-tabs-bordered d-flex"
+                  id="borderedTabJustified"
+                  role="tablist"
+                >
+                  <li className="nav-item flex-fill" role="presentation">
+                    <button
+                      className="nav-link w-100 active"
+                      id="home-tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#bordered-justified-home"
+                      type="button"
+                      role="tab"
+                      aria-controls="home"
+                      aria-selected="true"
+                    >
+                      Info Items
+                    </button>
+                  </li>
+                  <li className="nav-item flex-fill" role="presentation">
+                    <button
+                      className="nav-link w-100 "
+                      id="profile-tab"
+                      data-bs-toggle="tab"
+                      data-bs-target="#bordered-justified-profile"
+                      type="button"
+                      role="tab"
+                      aria-controls="profile"
+                      aria-selected="false"
+                    >
+                      Bids (32 bids)
+                    </button>
+                  </li>
+                </ul>
+                <div
+                  className="tab-content pt-2"
+                  id="borderedTabJustifiedContent"
+                >
+                  <div
+                    className="tab-pane fade show active"
+                    id="bordered-justified-home"
+                    role="tabpanel"
+                    aria-labelledby="home-tab"
+                  >
+                    <div>
+                      <small>High Bids</small>
+                      <button className="btn rounded-3 border">
+                        <i className="bi bi-heart me-2"></i> WatchList
+                      </button>
+                    </div>
+                  </div>
+                  <div
+                    className="tab-pane fade"
+                    id="bordered-justified-profile"
+                    role="tabpanel"
+                    aria-labelledby="profile-tab"
+                  >
+                    Nesciunt totam et. Consequuntur magnam aliquid eos nulla
+                    dolor iure eos quia. Accusantium distinctio omnis et atque
+                    fugiat. Itaque doloremque aliquid sint quasi quia distinctio
+                    similique. Voluptate nihil recusandae mollitia dolores. Ut
+                    laboriosam voluptatum dicta.
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
